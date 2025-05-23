8789cf90cae2e8f388338e48b430df16cea7b2e6
itsmefionah <santuafionah.05@gmail.com>
2025-05-23 19:46:45 +0800

    upload activity 15

--- Activity 15/activity15/resources/views/apiView.Blade.php
+++ Activity 15/activity15/resources/views/apiView.Blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Joke API</title>
-    <!-- Bootstrap CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
-    <!-- jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body class="bg-light">
